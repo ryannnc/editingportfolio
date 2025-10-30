18588c9177b922d0249f044911b76d78d695585d
rynnnnc <ry3ncao@gmail.com>
2025-10-30 16:22:26 -0400

    font and header

---
+++ portfolio/app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="w-full py-6 text-center var(--font-sf-pro)">
+      <h1 className="text-2xl text-black font-semibold dark:text-zinc-50">
+        Ryan Cao&apos;s Portfolio
+      </h1>
+    </header>
+  );
+}
---
+++ portfolio/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const sfPro = localFont({
+  src: [
+    { path: "./font/sfprodisplayregular.otf", weight: "400", style: "normal" },
+    { path: "./font/sfprodisplaymedium.otf", weight: "500", style: "normal" },
+    { path: "./font/sfprodisplaybold.otf", weight: "700", style: "normal" },
+  ],
+  variable: "--font-sf-pro",
+  display: "swap", // optional but recommended
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Ryan Cao’s Editing Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${sfPro.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
